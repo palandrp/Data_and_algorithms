41c0b83fb0c32ac702781d9d2b7584dceb9d4524
palandr <palandrp@gmail.com>
2017-05-22 13:56:50 +0500

    Исправленное и улучшенное дз2

--- c1hw2.c
+++ c1hw2.c
@@ -322,7 +322,7 @@ void showMax(int range) {
 }
 
 	/*
-	*	Функция генерации рандомного числа с помощью пользователя
+	*	Функция генерации рандомного числа
 	*/
 void genRealRandom() {
 
@@ -334,32 +334,13 @@ void genRealRandom() {
 	unsigned long int ttime;
 	ttime = time (NULL);
 
-	/* ============================================
-	*  Переворачиваем пепеменную ttime задом наперед
-	*  берем остатоки от деления, формируем новое число
-	*  с меняющейся верхней частью (а не нижней, как у ttime)
-	*/
-	int ptime;
-	int count = 0;
-	int array[50];
-	unsigned int Xn = 0;
-
-	while (ttime > 0) {
-		ptime = ttime % 10;
-		ttime = ttime / 10;
-		array[count] = ptime;
-		// printf("%d", array[count]);  // отладочное сообщение
-		count++;
+	int ptime = 0;
+	for (int i = 0; i < 2; i++) {
+		ptime = ptime * 10 + ttime % 10;
+		ttime /= 10;
 	}
 
-	Xn = array[0];
-	for (int i = 1; i < 2; i++) {	// берем только две первые цифры,
-		Xn = Xn *10 + array[i];		// которые меняются быстрее всего
-	}
-	// printf("\n%d\n", Xn);  // отладочное сообщение
-	//==============================================
-
-	random = (a * Xn + b) % m;
+	random = (a * ptime + b) % m;
 
 	printf("%d\n", random);
 }
