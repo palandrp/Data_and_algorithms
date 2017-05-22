4ab4fee6d41a40ae476278309b93b1f441071b54
palandr <palandrp@gmail.com>
2017-05-22 13:56:33 +0500

    Улучшенный рандом

--- random.c
+++ random.c
@@ -12,32 +12,14 @@ int main(int argc, char const *argv[])
 	unsigned long int ttime;
 	ttime = time (NULL);
 
-	/* ============================================
-	*  Переворачиваем пепеменную ttime задом наперед
-	*  берем остатоки от деления, формируем новое число
-	*  с меняющейся верхней частью (а не нижней, как у ttime)
-	*/
-	int ptime;
-	int count = 0;
-	int array[50];
-	unsigned int Xn = 0;
-
-	while (ttime > 0) {
-		ptime = ttime % 10;
-		ttime = ttime / 10;
-		array[count] = ptime;
-		// printf("%d", array[count]);  // отладочное сообщение
-		count++;
+	int ptime = 0;
+	for (int i = 0; i < 2; i++) {
+		ptime = ptime * 10 + ttime % 10;
+		ttime /= 10;
 	}
+	// printf("%ld : %d\n", ttime, ptime);  // отладочное сообщение
 
-	Xn = array[0];
-	for (int i = 1; i < 2; i++) {	// берем только две первые цифры,
-		Xn = Xn *10 + array[i];		// которые меняются быстрее всего
-	}
-	// printf("\n%d\n", Xn);  // отладочное сообщение
-	//==============================================
-
-	random = (a * Xn + b) % m;
+	random = (a * ptime + b) % m;
 
 	printf("%d\n", random);
 
