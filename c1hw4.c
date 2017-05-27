f79f245183d327306f4baa54ad4aa914e6014348
palandr <palandrp@gmail.com>
2017-05-27 21:26:35 +0500

    Исправленно, теперь работает

--- c1hw4.c
+++ c1hw4.c
@@ -4,25 +4,25 @@
  * @author Pavel Petrikovskiy
  * @version 22.05.17.
  */
+
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
 
-#define MaxN 100
-#define D char
+#define MaxN 10
 
-// ввод массива с клавиатуры
-
-void toCharArray(char array[]) {
-	char end = -1;
-	int i = 0;
-	while (end != '0') {
-		scanf("%s", &array[i]);
-		end = array[i];
-		i++;
+/**
+ * Печатаем массив.
+ */
+void printArray(int array[], int M){
+	for (int i = 0; i < M; ++i){
+		printf("%d ", array[i]);
 	}
 }
 
+/**
+ * Собственная функция рандома.
+ */
 int genRealRandom() {
 	int m = 101;
 	int b = 1;
@@ -40,6 +40,10 @@ int genRealRandom() {
 	return (a * ptime + b) % m;
 }
 
+/**
+ * Собственная функция паузы.
+ * Параметр в секундах.
+ */
 void sleep(int _time) {
 	unsigned long ntime = 0;
 	unsigned long ttime = time(NULL);
@@ -49,88 +53,112 @@ void sleep(int _time) {
 	}
 }
 
+/**
+ * a. Ввод массива с клавиатуры.
+ */
+void toArray(int array[], int M) {
+	for (int i = 0; i < M; i++) {
+		scanf("%d", &array[i]);
+	}
+}
+
+/**
+ * b. Заполняем массив случайными числами.
+ * Заполнение происходит автоматически.
+ * Пауза в секунду нужна, чтобы сгенерировать
+ * разные случайные числа.
+ */
 void ramdomArray(int array[], int M) {
+	printf("#");
 	for (int i = 0; i < M; i++) {
 		srand(time(NULL));
 		array[i] = genRealRandom();
 		sleep(1);
+		printf("=");
+		fflush(stdout);
 	}
 	array[M+1] = 0;
 	printf("\n");
 }
 
-int arrayLength(D array[]) {
-	int count = 0, i = 0;
-	while (array[i] != '0') {
-		count++;
-		i++;
+/**
+ * c. Подсчитываем сумму элементов массива.
+ * Предполагается, что в массиве цифры (int)
+ */
+int findSumm(int array[], int M){
+	int sum = 0;
+	for (int i = 0; i < M; i++){
+		sum += array[i];
 	}
-	return count;
+	return sum;
 }
 
-int averageSumm(int array[]) {
-	int count = 0, i = 0, sum;
-	while (array[i] != 0){
+/**
+ * d. Подсчитываем среднеарифметическую сумму элементов 
+ * массива.
+ */
+int averageSumm(int array[], int M){
+	int sum = 0, i = 0, count = 0;
+	for (; i < M; i++){
 		sum += array[i];
-		i++;
-	}
-	i = 0;
-	while (array[i] != 0) {
 		count++;
-		i++;
 	}
 	return sum/count;
 }
 
-int findFirst(int array[]){
-	int count = 0, i = 0, sum, diff, max;
-	while (array[i] != 0){
-		sum += array[i];
-		i++;
-	}
-	i = 0;
-	while (array[i] != 0) {
-		count++;
-		i++;
-	}
-	i = 0;
-	max = sum - array[0];
-	while(array[i] != 0){
-		if (max < sum - array[i]){
-			max = sum - array[i];
-			diff = array[i];
+/**
+ * e. Находим номер самого отличающегося от средней суммы
+ * элемента.
+ */
+int findFirst(int array[], int M){
+	int diff, max = 0;
+	int av_sum = averageSumm(array, M);
+	for (int i = 0; i < M; i++){
+		if (max < av_sum - array[i]){
+			max = av_sum - array[i];
+			diff = i; 
 		}
 	}
 	return diff;
 }
 
-int findMax(int array[]){
-	int i = 0, max = array[0];
-	if (max < array[i]) max = array[i];
+/**
+ * f. Находим максимальный элемент в массиве.
+ */
+int findMax(int array[], int M){
+	int max = array[0];
+	for (int i = 0; i < M; i++){
+		if (max < array[i]) max = array[i];
+	}
 	return max;
 }
 
-
 int main(int argc, char const *argv[]) {
 
-	char array0[MaxN];
-	toCharArray(array0);
-	printf("%s\n", array0);
+	printf("\nВведите цифры массива int, нажимая Enter.\n");
+	printf("Всего 10 цифр:\n");
+	int array0[MaxN];
+	toArray(array0, MaxN);
+	printf("Выводим полученный массив:\n");
+	printArray(array0, MaxN);
 
+	printf("\n\nВторое задание. Генерируется массив случайных чисел:\n");
 	int array1[MaxN];
-	int count = 10;
-	ramdomArray(array1, count);
-	for (int i; i < count; i++) {
-		printf("%i\n", array1[i]);
-	}
+	ramdomArray(array1, MaxN);
+	printf("Теперь этот массив выводится:\n");
+	printArray(array1, MaxN);
 
-	D array2[] = {'d','f','a','e','0'};
-	arrayLength(array2);
+	printf("\n\nПодсчитываем сумму элементов массива:\n");
+	printf("%d\n", findSumm(array1, MaxN));
 
-	printf("\n\n");
-	printf("%d\n", averageSumm(array1));
-	printf("%d\n", findMax(array1));
-	printf("%d\n", findFirst(array1));
+	printf("\nНаходим среднюю сумму элементов массива из второго задания:\n");
+	printf("%d\n", averageSumm(array1, MaxN));
+
+	printf("\nНаходим номер максимально отличного от неё элемента:\n");
+	printf("[%d]\n", findFirst(array1, MaxN));
+
+	printf("\nНаходим максимальный элемент массива:\n");
+	printf("%d\n", findMax(array1, MaxN));
 
 	return 0;
 }
