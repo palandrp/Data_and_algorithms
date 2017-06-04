a6d05c342dbec0a22122659bc2fb6d0c8b3886c4
palandr <palandrp@gmail.com>
2017-06-04 12:49:19 +0500

    Приступаем к 7-й дз

---
+++ c1hw7.c
@@ -0,0 +1,16 @@
+/**
+ * Algorithms and data structure
+ *
+ * @author Pavel Petrikovskiy
+ * @version 04.06.17.
+ */
+
+#include "c1hw6.h"
+#include <string.h>
+
+/**
+*
+*/
+
+//###### Задание№1 #######
+//######## Begin #########
