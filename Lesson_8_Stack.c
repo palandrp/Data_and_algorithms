023d28fe889982ba49a86507d78ec5c88110438b
palandr <palandrp@gmail.com>
2017-06-18 23:47:36 +0500

    Пример организации стека

--- Lesson_8_Stack.c
+++ Lesson_8_Stack.c
@@ -1,27 +1,33 @@
-#include   <stdio.h> 
+#include <stdio.h> 
 
-#define   T   char
-#define   MaxN   1000 T   Stack[ MaxN ];
+#define T char
+#define MaxN 1000 T Stack[MaxN];
 
-int   N=- 1 ;
-void   push( T   i) {
- if   (N< MaxN )
- {
- N++;
- }  Stack[N]=i;
-}  else   printf( "Stack   overflow" );
-T   pop() {
- if   (N!=- 1 )   return   Stack[N--];
- else   printf( "Stack   is   empty" );
+int N= -1;
+
+void push(T i){
+	if (N < MaxN){
+		N++;
+		Stack[N] = i;
+	} else 
+		printf("Stack overflow");
 }
-int   main( int   argc,   char   *argv[]) {
- T   c;
- push( 'a' );
- push( 'b' );
- push( 'c' );
- push( 'd' );
- push( 'e' );
- push( 'f' );
- while (N!=- 1 )   printf( "%c" ,pop());
- return 0;
+
+T pop(){
+	if   (N != -1)
+		return Stack[N--];
+	else 
+		printf("Stack is empty");
+}
+
+int main(int argc,char *argv[]){
+	T c;
+	push('a');
+	push('b');
+	push('c');
+	push('d');
+	push('e');
+	push('f');
+	while (N != -1) printf("%c",pop());
+	return 0;
 }
