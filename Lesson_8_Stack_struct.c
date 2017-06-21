ed0f13a0705507ba9ec6a8aeeb9402b5c86d3a8b
palandr <palandrp@gmail.com>
2017-06-21 23:54:03 +0500

    Стек на основе односвязного списка

--- Lesson_8_Stack_struct.c
+++ Lesson_8_Stack_struct.c
@@ -15,3 +15,16 @@ struct Stack Stack;
 
 Stack.maxSize = 100;
 Stack.head = NULL;
+
+if ( Stack . size >= Stack . maxSize )  { 
+	printf ( "Error stack size" );
+	 return;
+} 
+
+Node   * tmp;
+
+tmp =( Node *) malloc ( sizeof ( Node ));
+
+tmp -> value = value ;  // записываем данные 
+tmp -> next = Stack . head ; // записываем предыдущее значение главы
+
