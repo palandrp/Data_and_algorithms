6f2eff200939a3d49bfc6b0f2309bf37c4a1a512
palandr <palandrp@gmail.com>
2017-06-22 15:29:17 +0500

    Стек с использованием структуры

--- Lesson_8_Stack_struct.c
+++ Lesson_8_Stack_struct.c
@@ -1,30 +1,70 @@
+#include <stdio.h>
+#include <malloc.h>
+#define T char
+
+// Опишем структуру узла списка 
 struct TNode {
-	int value;  // Данные
-	struct TNode * next; // Указатель на следующий элемент списка
-};
-
-typedef struct TNode Node;
-
-struct Stack {
-	Node * head;
-	int size;
-	int maxSize;
+	T value;
+	struct TNode* next;
 }; 
-
+// Чтобы не писать везде struct TNode дадим этой структуре имя Node
+typedef struct TNode Node; 
+struct Stack {
+ 	Node* head;
+ 	int size;
+}; 
+int maxSize;
 struct Stack Stack;
 
-Stack.maxSize = 100;
-Stack.head = NULL;
+void push(T value){
+ 	if(Stack.size >= Stack.maxSize){
+ 		printf("Error stack size");
+		return;
+	}
+	Node* tmp = (Node*) malloc(sizeof(Node)); 
+	tmp->value = value;
+	tmp->next = Stack.head;
+	Stack.head = tmp;
+	Stack.size ++;
+}
 
-if ( Stack . size >= Stack . maxSize )  { 
-	printf ( "Error stack size" );
-	 return;
-} 
+// Выталкиваем из списка
+T pop(){
+ 	if(Stack.size == 0){
+ 		printf("Stack is empty");
+ 		return;
+	}
+	// Временный указатель
+ 	Node* next = NULL;
+	// Значение "наверху" списка
+	T value; 
+	value = Stack.head->value; 
+	next = Stack.head; 
+	Stack.head = Stack.head->next;
+	// Запись, на которую указывала голова удаляем, освобождая память
+	free(next);
+	// Возвращаем значение, которое было в голове
+ 	Stack.size--; 
+ 	return value;
+}
 
-Node   * tmp;
+void PrintStack(){
+ 	Node* current = Stack.head; 
+ 	while(current != NULL){
+		printf("%c " , current->value);
+		current = current->next; 
+	}
+}
 
-tmp =( Node *) malloc ( sizeof ( Node ));
-
-tmp -> value = value ;  // записываем данные 
-tmp -> next = Stack . head ; // записываем предыдущее значение главы
-
+int main(int argc,char* argv[]){
+ 	Stack.maxSize = 100; 
+ 	Stack.head = NULL; 
+ 	push('a');
+	push('b');
+	push('c'); 
+	push('d'); 
+	push('e'); 
+	push('f'); 
+	PrintStack();
+	return 0;
+}
